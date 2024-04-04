63a746662bb283680363e35ffcfeb6699fb3af5e
leshakrot <85496308+leshakrot@users.noreply.github.com>
2024-04-04 11:41:29 +0300

    instancing, batching

---
+++ Assets/Scripts/GPUInstancingEnabler.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class GPUInstancingEnabler : MonoBehaviour
+{
+    private void Awake()
+    {
+        MaterialPropertyBlock materialPropertyBlock = new MaterialPropertyBlock();
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.SetPropertyBlock(materialPropertyBlock);
+    }
+}
